ead4d23c7fbdfbd8f7a6a71d3f0cad7c0a8fd3cc
barsu5489 <ebarsulai@gmail.com>
2022-12-26 18:09:17 +0300

    combined all the three pillars

--- src/index.js
+++ src/index.js
@@ -35,14 +35,76 @@ function renderToys(toys){
     cardDiv.appendChild(image)
     cardDiv.appendChild(p)
     cardDiv.appendChild(btn)
+    function patchLikes(){
+    
+      btn.addEventListener('click', ()=>{
+        console.log('clicked')
+
+  fetch(`http://localhost:3000/toys/${toy.id}`,{
+    method:'PATCH',
+    headers:
+    {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    
+    body: JSON.stringify({
+      "likes": toy.likes ++
+    })
 
   })
+  .then(res=>res.json())
+  .then(like=>{
+    console.log(like)
+  })
+       
+      })
+    }
+    patchLikes()
+
+  })
+  postToy()
 }
+
+
+
+
+
+
+function postToy(){
+const toyForm = document.querySelector('.add-toy-form')
+toyForm.addEventListener('submit',e=>{
+  e.preventDefault()
+  const name = e.target.name.value
+  const  image = e.target.image.value
+
+  const data = {
+    name:name,
+    image:image,
+    likes:0
+  }
+  fetch('http://localhost:3000/toys',{
+    method:'POST',
+    headers:
+    {
+      "Content-Type": "application/json",
+      'Accept': "application/json"
+    },
+    body:JSON.stringify(data)
+  })
+  .then(res=>res.json())
+  .then(to=>{
+    console.log(to)
+  })
+})
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
+    
     toys()
     addToy = !addToy;
     if (addToy) {
